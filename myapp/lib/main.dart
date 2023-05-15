e8a0cf6c905f163c7442b79e6a7288e3d78805df
karimhijazi1 <karimhijazi675@gmail.com>
2023-05-16 01:21:31 +0300

    first game

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/screens/game.dart';
 import 'package:myapp/screens/home.dart';
+import 'package:myapp/theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // theme: customTheme(),
+      theme: customTheme(),
       initialRoute: '/',
       routes: {
         '/': (context) => const Home(),
